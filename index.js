5a2f662782a43e577174600383804c7991137f1e
HazelHappy2024 <liu.shuru@northeastern.edu>
2024-12-11 13:23:37 -0800

    addusermodelschema

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
+import "dotenv/config";
 import WorkingWithObjects from "./Lab5/WorkingWithObjects.js";
 import WorkingWithArrays from "./Lab5/WorkingWithArrays.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
@@ -11,9 +12,14 @@ import session from "express-session";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
-
+import mongoose from "mongoose";
 
 //const express = require("express");
+
+console.log("Environment Variables:", process.env.MONGO_CONNECTION_STRING);
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(
