db714a9c83501a4c47007ed59807041fa6429791
HazelHappy2024 <liu.shuru@northeastern.edu>
2024-12-12 15:11:22 -0800

    modifycors

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    //origin: process.env.NETLIFY_URL || "http://localhost:3000",
-    origin:"http://localhost:3000",
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    //origin:"http://localhost:3000",
   })
 );
 
