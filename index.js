800021dfaf903603adc91c898a3c80cf41b8bf10
HazelHappy2024 <liu.shuru@northeastern.edu>
2024-12-12 20:19:22 -0800

    remotedeploy

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-// const express = require('express')
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
 import HelloRoutes from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
-import PathParameters from "./Lab5/PathParameters.js";
-import QueryParameters from "./Lab5/QueryParameters.js";
 import UserRoutes from "./Kanbas/Users/routes.js";
-import WorkingWithObjects from "./Lab5/WorkingWithObjects.js";
-import WorkingWithArrays from "./Lab5/WorkingWithArrays.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
@@ -17,25 +12,31 @@ import session from "express-session";
 import EnrollmentsRoutes from "./Kanbas/Enrollments/routes.js";
 
 console.log("Environment Variables:", process.env.MONGO_CONNECTION_STRING);
-console.log("Environment Variables:", process.env);
-
 
 const CONNECTION_STRING =
-process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+  process.env.MONGO_CONNECTION_STRING || "mongodb+srv://liushurui0106:1993Apple@kanbas.qblgw.mongodb.net/?retryWrites=true&w=majority&appName=Kanbas";
 mongoose.connect(CONNECTION_STRING);
-
-
-
 
 const app = express();
 
-const reactUrl = process.env.REACT_URL || 'http://localhost:3001';
-const corsOptions = {
-  origin: reactUrl,
-  methods: ['GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  credentials: true,
-};
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        process.env.NETLIFY_URL,
+        "http://localhost:3000",
+  
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
 
 
 const sessionOptions = {
@@ -51,27 +52,16 @@ if (process.env.NODE_ENV !== "development") {
     domain: process.env.NODE_SERVER_DOMAIN,
   };
 }
-
-mongoose
-  .connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected successfully"))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
 app.use(session(sessionOptions));
 
 app.use(express.json());
 
-HelloRoutes(app);
 Lab5(app);
-PathParameters(app);
-QueryParameters(app);
-WorkingWithObjects(app);
-WorkingWithArrays(app);
+HelloRoutes(app);
 UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 EnrollmentsRoutes(app);
-
 
 app.listen(process.env.PORT || 4000);
